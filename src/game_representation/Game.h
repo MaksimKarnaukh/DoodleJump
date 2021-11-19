c201bef0efaf0eedb062b0bdc8aa43ad133e517a
Maksim <Maksim.Karnaukh@student.uantwerpen.be>
2021-11-19 01:45:59 +0100

    Added doodle sprites to assets/DoodleJumper and addes methodes to Game.h class

--- src/game_representation/Game.h
+++ src/game_representation/Game.h
@@ -5,8 +5,21 @@
 #ifndef DOODLEJUMP_GAME_H
 #define DOODLEJUMP_GAME_H
 
+#include "Window.h"
+
 class Game {
 
+public:
+    Game();
+    ~Game();
+    void HandleInput();
+    void Update();
+    void Render();
+    Window* GetWindow();
+
+private:
+    void MoveMushroom();
+    Window m_window;
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,14 @@
 
 int main() {
 
-    Game game;
+    // Program entry point.
+    Game game; // Creating our game object.
+    while(!game.GetWindow()->IsDone()){
+        // Game loop.
+        game.HandleInput();
+        game.Update();
+        game.Render();
+    }
+
     return 0;
 }
